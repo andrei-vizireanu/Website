474ee7832fe179240ed18de1190a99866b5fd08b
andrei-vizireanu <59308569+andrei-vizireanu@users.noreply.github.com>
2021-10-15 17:58:19 +0300

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -1,56 +1,19 @@
-function home() {
-  document.getElementById("about-me").style.display = "none";
-  document.getElementById("home").style.display = "block";
-  document.getElementById("about-me-button").className = "special-text";
-  document.getElementById("home-button").className = "on-page";
-}
-
-function aboutMe() {
-  document.getElementById("about-me").style.display = "block";
-  document.getElementById("home").style.display = "none";
-  document.getElementById("about-me-button").className = "on-page";
-  document.getElementById("home-button").className = "special-text";
-}
-
 // initiate the fonts variable
-var font = new FontFace("Montserrat", "url(fonts/montserrat-v15-latin-500.woff2)", {
-  style: 'normal', unicodeRange: 'U+000-5FF', weight: '500'
-});
-
-// display the desktop header
-if (window.matchMedia('(min-width: 800px)').matches) {
-  var content = document.getElementById("mobile-menu");
-  content.style.display = "none";
-
-  // load the fonts and after that display the header
-  font.load().then(function() {  
-    var content = document.getElementById("desktop");
-    content.style.display = "block";
-  });
-
-}
-
-// display the mobile header
-if (window.matchMedia('(max-width: 800px)').matches) {
-  var content = document.getElementById("desktop");
-  content.style.display = "none";
-  // load the fonts and after that display the header
-  font.load().then(function() {  
-    var content = document.getElementById("mobile-menu");
-    content.style.display = "block";
-  });
-}
-
+// var font = new FontFace("Montserrat", "url(fonts/montserrat-v15-latin-500.woff2)", {
+//   style: 'normal', unicodeRange: 'U+000-5FF', weight: '500'
+// });
 
 // load the fonts and after that display the header
-// font.load().then(function() {
-
-//   var content = document.getElementById("content-load");
+// font.load().then(function() {  
+  
+//   var content = document.getElementById("mobile-menu");
+//   content.style.display = "block";
+//   var content = document.getElementById("desktop");
 //   content.style.display = "block";
 
 // });
 
-// display the desktop header
+// // display the desktop header
 // if (window.matchMedia('(min-width: 800px)').matches) {
 //   var content = document.getElementById("mobile-menu");
 //   content.style.display = "none";
@@ -58,7 +21,19 @@ if (window.matchMedia('(max-width: 800px)').matches) {
 //   content.style.display = "block";
 // }
 
+// // display the mobile header
+// if (window.matchMedia('(max-width: 800px)').matches) {
+//   var content = document.getElementById("desktop");
+//   content.style.display = "none";
+//   var content = document.getElementById("mobile-menu");
+//   content.style.display = "block";
+//   console.log("mobile");
+// }
 
+function mobileNavIcon(){
+  
+  console.log("Nav clicked");
+}
 
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
@@ -72,7 +47,3 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });
-
-
-//setTimeout(() => {  document.getElementById("content-load").style.display = "block" }, 500);
-
